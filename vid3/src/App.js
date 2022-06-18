26315ab276b2c28fe2ddce6cbdadab76e8fccee3
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-18 16:20:08 +0100

    use useState

--- vid3/src/App.js
+++ vid3/src/App.js
@@ -6,18 +6,23 @@ import axios from 'axios';
 const URL ="https://raw.githubusercontent.com/getfutureproof/fp_study_notes_hello_github/main/auguste/roster.json"
 
 function App() {
+  const[students, setStudents] = useState([]) ;
+
+
   useEffect(() => {
 
-
     const fetchStudents = async () => {
-
-      const {data} = await axios.get(URL)
-   
-      console.log(data)
+      const { data } = await axios.get(URL)
+      setStudents(data)
     }
+
     fetchStudents()
 
   }, [])
+
+  console.log(students)
+
+
   return (
     <div className="App">
       <header className="App-header">
