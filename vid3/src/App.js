769bb4348130682aea30f4e0c2f678ddae1dec5b
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-18 16:04:34 +0100

    install axios and connect URL

--- vid3/src/App.js
+++ vid3/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
 import React, { useEffect } from 'react';
+import axios from 'axios';
 
-// const URL ="https://raw.githubusercontent.com/getfutureproof/fp_study_notes_hello_github/main/auguste/roster.json"
+
+const URL ="https://raw.githubusercontent.com/getfutureproof/fp_study_notes_hello_github/main/auguste/roster.json"
 
 function App() {
+  useEffect(() => {
+     const response = axios.get(URL)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
